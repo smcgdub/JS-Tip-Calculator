b7b9432f67b9bef6b5344c12a5fad6bf907e7a58
Stephen Mc Govern <stephen_xyz1@hotmail.com>
2022-02-08 22:52:19 +0000

    Added the JS function to workout the tip and breakdown per person

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,24 @@
 // Buttons 
 const calculateBtn = document.getElementById('calculate_btn');
 const resetBtn = document.getElementById('reset_btn');
+
+calculateBtn.addEventListener('click', function(){
+  // Variables
+  let uiBillTotalInput = parseFloat(document.getElementById('bill_total_input').value);
+  let uiTipPercentInput = parseFloat(document.getElementById('tip_percent_input').value);
+  let uiNumberOfPeopleInput = parseInt(document.getElementById('number_of_people_input').value);
+
+  // Equations
+  // Tip amount
+  let tipAmount = document.getElementById('tip_percent_display').value = 
+  parseFloat((uiBillTotalInput) * parseFloat(uiTipPercentInput) / 100).toFixed(2);
+  // Total Bill
+  let uiBillTotalDisplay = document.getElementById('bill_total_display').value = 
+  parseFloat((uiBillTotalInput) + parseFloat(tipAmount)).toFixed(2);
+  // Price per person
+  let uiEachPersonPays = document.getElementById('each_person_pays_display').value = parseFloat(uiBillTotalDisplay / uiNumberOfPeopleInput).toFixed(2);
+  console.log("Total Bill" + uiBillTotalInput)
+  console.log("Tip Amount" + tipAmount)
+  console.log("Price per person" + uiEachPersonPays)
+  console.log("Total bill" + uiBillTotalDisplay)
+});
